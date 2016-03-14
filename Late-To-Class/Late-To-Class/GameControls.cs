d2b7a2f0610f859a4de7c6184ccc3b2181b3bc0a
Christopher Banks (RIT Student) <cdb6474@ad.rit.edu>
2016-03-14 14:19:16 -0400

    Made game controls class to handle variable input.

---
+++ Late-To-Class/Late-To-Class/GameControls.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Late_To_Class
+{
+    /// <summary>
+    /// Manager for holding configurable game controls
+    /// </summary>
+    class GameControls
+    {
+        //TODO: add content
+    }
+}
