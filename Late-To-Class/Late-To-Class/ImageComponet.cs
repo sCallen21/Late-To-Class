db9e0a08aea7e6d68202be9810480e64375a18cd
axk4647 <arturokuang@gmail.com>
2016-03-26 15:32:07 -0400

    Menu stuff

---
+++ Late-To-Class/Late-To-Class/ImageComponet.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Late_To_Class
+{
+    class ImageComponet
+    {
+        public enum DrawMode
+        {
+            center = 1, Stretch,
+        };
+
+        private Texture2D texture;
+        private DrawMode drawmode;
+        private Rectangle rect;
+        private SpriteBatch spritebatch = null;
+
+
+
+    }
+}
