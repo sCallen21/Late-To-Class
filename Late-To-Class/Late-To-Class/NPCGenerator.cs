f772184b98983dcb066afc4d4a6a2f7580f7ece9
Christopher Banks (RIT Student) <cdb6474@ad.rit.edu>
2016-05-11 14:48:46 -0400

    Re-added the NPC maker

---
+++ Late-To-Class/Late-To-Class/NPCGenerator.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+ using Microsoft.Xna.Framework.Content;
+ using Microsoft.Xna.Framework.Graphics;
+ using System.Collections.Generic;
+ using System.Linq;
+ using System.Text;
+ 
+ namespace Late_To_Class
+ {
+     class NPCGenerator
+     {
+         ContentManager Content;
+         NPC person;
+         Texture2D builder;
+         List<NPC> people = new List<NPC>();
+ 
+        public List<NPC> CreateSpawn(ContentManager Content)
+         {
+             this.Content = Content;
+             builder = Content.Load<Texture2D>("NPCSheet");
+             Random rand = new Random();
+             int nClusterSize = rand.Next(1,6);
+             for(int i = 0; i < nClusterSize; i++)
+             {
+                 person = new NPC();
+                 Create(person);
+                 people.Add(person);
+             }
+ 
+             return people;
+         }
+ 
+         public void Create(NPC human)
+        {
+            Random rand = new Random();
+            int nHairStyle = rand.Next(0, 4);
+            int nBodyStle = rand.Next(0, 6);
+            int red = rand.Next(0, 256);
+            int green = rand.Next(0, 256);
+            int blue = rand.Next(0, 256);
+            human.SetContent(nBodyStle, nHairStyle, red, green, blue, builder);        
+        }
+     }
+ }
